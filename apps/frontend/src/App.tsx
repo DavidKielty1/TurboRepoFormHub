3aa8b2f892898456c814ddb82b1ffda85ab09392
David Kielty <davidkielty@live.co.uk>
2024-01-25 14:34:43 +0000

    Updated queue service to use graphql

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,22 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./App.css";
 import Dashboard from "./screens/Dashboard";
+
+const URL = process.env.GRAPHQL_HOST
+  ? `https://${process.env.GRAPHQL_HOST}/graphql`
+  : "http://localhost:8080/graphql";
+
+const client = new ApolloClient({
+  uri: URL,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Dashboard />
+      <ApolloProvider client={client}>
+        <Dashboard />
+      </ApolloProvider>
     </>
   );
 }
