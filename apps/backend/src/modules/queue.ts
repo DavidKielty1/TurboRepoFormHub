7796952ed7b3094e3f47d14d3b077c211747070d
David Kielty <davidkielty@live.co.uk>
2024-01-25 12:25:58 +0000

    Adding type: web to render.yaml config

--- apps/backend/src/modules/queue.ts
+++ apps/backend/src/modules/queue.ts
@@ -5,9 +5,14 @@ import { Redis } from 'ioredis';
 const QUEUE_NAME = 'default';
 
 if (!process.env.REDIS_URL) {
-  console.warn('REDIS_URL is not defined');
+  console.warn('REDIS_URL is not defined!');
 }
-const connection = new Redis(process.env.REDIS_URL);
+const connectionOptions = {
+  maxRetriesPerRequest: null,
+  // Add any other necessary options here
+};
+
+const connection = new Redis(process.env.REDIS_URL, connectionOptions);
 
 export const queue = new Queue(QUEUE_NAME, { connection });
 
