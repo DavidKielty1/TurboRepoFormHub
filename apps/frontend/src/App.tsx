59ec80c73403e10ba0c53af5b7dd257486f9b991
David Kielty <davidkielty@live.co.uk>
2024-01-25 16:17:17 +0000

    Render cleanup

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,6 +2,10 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./App.css";
 import Dashboard from "./screens/Dashboard";
 
+if (!process.env.REACT_APP_GRAPHQL_HOST) {
+  console.log("No reactGraphQLhost");
+}
+
 const URL = process.env.REACT_APP_GRAPHQL_HOST
   ? `https://${process.env.REACT_APP_GRAPHQL_HOST}.onrender.com/graphql`
   : "http://localhost:8080/graphql";
